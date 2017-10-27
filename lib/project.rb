6f7bd96bdb8be17cfbd7a06db47e590d6cd49d88
Monica  Karasek <mlkarasek@gmail.com>
2017-10-27 23:31:02 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,16 +1,17 @@
 class Project
-
   attr_accessor :backers, :title
-
 
   def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(backer)
-    add_backer << backer
-    backer.backed_projects << self
-  end 
+  def backers
+    @backers
+  end
 
+  def add_backer(backer)
+    @backers << backer
+    backer.backed_projects << self
+  end
 end
